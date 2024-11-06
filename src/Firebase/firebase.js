4215b77902714ab752fc6c76b16436ccb0c95c12
Victoras Isaakidis <victoras24@Victorass-MacBook-Air.local>
2024-11-06 20:53:10 +0200

    testing, not working on mobile due to firestore api key

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,13 +3,22 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyB-i_M1DXiBGA0xPoC2Numnbb5Sn20jXY4",
+  authDomain: "company-search-cy.firebaseapp.com",
+  projectId: "company-search-cy",
+  storageBucket: "company-search-cy.firebasestorage.app",
+  messagingSenderId: "195294741562",
+  appId: "1:195294741562:web:267b2482be1a97a8c76103",
 };
 
 const app = initializeApp(firebaseConfig);
